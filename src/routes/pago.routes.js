070911c1aaeab6fdbaa2b32a30088574e0dd0ddb
Rodrigo Paz <rodripaz45@gmail.com>
2024-05-14 17:14:06 -0400

    comeback

--- src/routes/pago.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { handlePagoNotification } from '../controllers/pago.controller.js';
-
-const router = Router();
-
-router.post('/pago/notification', handlePagoNotification);
-
-export default router;

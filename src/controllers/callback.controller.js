cc8f0b4ca87341c12a41dd569a81db3a7e56b69b
Rodrigo Paz <rodripaz45@gmail.com>
2024-05-16 14:14:46 -0400

    callback v2

--- src/controllers/callback.controller.js
+++ src/controllers/callback.controller.js
@@ -1,14 +1,20 @@
 export const confirmarPago = async (req, res) => {
     console.log('Solicitud recibida en /confirmar_pago');
-    const data = req.body;
-    console.log('Datos recibidos:', data);
+    
+    try {
+        const data = req.body;
+        console.log('Datos recibidos:', JSON.stringify(data, null, 2));
 
-    if (data.error === 0 && data.status === 1) {
-        const pedidoID = data.PedidoID;
-        console.log('Pago confirmado para el pedido ID:', pedidoID);
-        res.json({ message: 'Pago confirmado' });
-    } else {
-        console.warn('Pago no confirmado:', data);
-        res.status(400).json({ error: 'Pago no confirmado' });
+        if (data.error === 0 && data.status === 1) {
+            const pedidoID = data.PedidoID;
+            console.log('Pago confirmado para el pedido ID:', pedidoID);
+            res.json({ message: 'Pago confirmado' });
+        } else {
+            console.warn('Pago no confirmado:', JSON.stringify(data, null, 2));
+            res.status(400).json({ error: 'Pago no confirmado' });
+        }
+    } catch (error) {
+        console.error('Error procesando la solicitud:', error);
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 };

139a449cece97f2940909dd4eaa76d139bbaa48b
Rodrigo Paz <rodripaz45@gmail.com>
2024-05-16 13:41:09 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ const app = express();
 
 app.use(express.json());
 
-// Configuración de CORS
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(userroutes);
 app.use(pacienteroutes);
